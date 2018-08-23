6da0d3ae97091d4b6cede0977ed19eac30c7dff4
DevinSyas <42630000+DevinSyas@users.noreply.github.com>
2018-08-23 14:30:39 -0400

    Assignment1

---
+++ TextEditorList/src/texteditorlist/TextEditorList.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package texteditorlist;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author devin_000
+ */
+public class TextEditorList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TextEditorFrame frame = new TextEditorFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    
+}
